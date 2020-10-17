4be23f011de1d8082a1c338a50dd95e88ceb01f5
muthanna37 <73015959+muthanna37@users.noreply.github.com>
2020-10-17 14:00:32 +0530

    Create quessinggame.sh

---
+++ quessinggame.sh
@@ -0,0 +1,19 @@
+function guess(){
+true_ans=$(ls -l |grep "^-"|wc -l)
+while true;
+do
+  echo "please enter your guess"
+  read number
+  if [ $number -lt $true_ans ]
+  then
+    echo "your guess is less then the true number"
+  elif [ $number -gt $true_ans ]
+  then 
+     echo "your guess is greater then the true number" 
+  else
+     echo "congragulation ,u are right."
+  break;
+  fi
+done
+}
+echo "guess the file in the current directory"
